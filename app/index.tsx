036871e75c57808e4c477336e34c155dd76e3572
TJ-Yusuke <jeeproud@gmail.com>
2025-01-02 15:51:29 +0900

    fix: プロジェクト初期化

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.appTitle}>AIたんご帳</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appTitle: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
+});
