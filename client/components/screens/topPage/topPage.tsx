d2448baaf34683eb273ebdde2552ed054c53f051
TJ-Yusuke <jeeproud@gmail.com>
2025-01-20 23:21:25 +0900

    chore: 単語を追加するボタンを追加

--- client/components/screens/topPage/topPage.tsx
+++ client/components/screens/topPage/topPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StyleSheet, Alert, FlatList } from "react-native";
-import { AnimatedFAB } from "react-native-paper";
+import { AnimatedFAB, Button } from "react-native-paper";
 import { useTopPageViewModel } from "./topPageViewModel";
 import { WordCard } from "../../wordCard";
 import { useRouter } from "expo-router";
@@ -51,12 +51,27 @@ const TopPage = () => {
         style={styles.scrollView}
         data={words}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <WordCard
-            word={item}
-            onDeleteClick={(wordText) => openDeleteAlert(wordText)}
-          />
-        )}
+        renderItem={({ item, index }) => {
+          const isLastItem = index === words.length - 1;
+          if (isLastItem) {
+            return (
+              <Button
+                icon="plus"
+                onPress={() => router.push("/addWordModal")}
+                contentStyle={styles.addButtonContent} // ボタン全体のスタイル
+                labelStyle={styles.addButtonLabel}
+              >
+                単語を追加する
+              </Button>
+            );
+          }
+          return (
+            <WordCard
+              word={item}
+              onDeleteClick={(wordText) => openDeleteAlert(wordText)}
+            />
+          );
+        }}
         onScroll={onScroll}
       />
       <AnimatedFAB
@@ -93,6 +108,15 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 20,
     marginHorizontal: 20,
+  },
+  addButtonContent: {
+    height: 60, // ボタンの高さを大きく
+    justifyContent: "center", // 縦方向の中央揃え
+    marginBottom: 20,
+  },
+  addButtonLabel: {
+    fontSize: 18, // フォントサイズを大きく
+    fontWeight: "bold", // テキストを太字に
   },
 });
 
